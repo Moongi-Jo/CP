7ed25e00c2e6a53a2f5aa4b95ffc434e2402d01d
Moon <moongi05@naver.com>
2025-09-02 15:00:25 +0900

    팩토리얼 개수 - 수학문제

---
+++ CP_Basic/src/_1676.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class _1676 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x=0;
+		
+		while(n/5!=0) {
+			x += n/5;
+			n = n/5;
+		}
+		System.out.println(x);
+			
+		
+		
+		
+	}
+}
